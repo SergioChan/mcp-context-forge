fa41c5b2ee781bfca5a57c88b374724d59756b46
Madhav Kandukuri <madhav165@gmail.com>
2026-03-04 23:39:42 +0530

    fix: rename alembic migration file to reflect servers.use instead of tools.execute
    Signed-off-by: Madhav Kandukuri <madhav165@gmail.com>

--- tests/unit/mcpgateway/db/test_rbac_permission_backfill_migration.py
+++ tests/unit/mcpgateway/db/test_rbac_permission_backfill_migration.py
@@ -22,7 +22,7 @@ import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy.pool import StaticPool
 
-MIGRATION_MODULE = "mcpgateway.alembic.versions.abf8ac3b6008_add_admin_overview_and_tools_execute_to_"
+MIGRATION_MODULE = "mcpgateway.alembic.versions.abf8ac3b6008_add_admin_overview_and_servers_use_to_"
 EXPECTED_REVISION = "abf8ac3b6008"
 EXPECTED_DOWN_REVISION = "d9e0f1a2b3c4"
 
